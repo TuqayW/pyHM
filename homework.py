422177f977fb26c3c0f66b13f851eff87e5918b3
TuqayW <101931875+TuqayW@users.noreply.github.com>
2023-09-29 07:15:53 -0700

    Main
    Main

---
+++ homework.py
@@ -0,0 +1,29 @@
+# #Tapsiriq 1
+# input="I like banana"
+# print(input.replace("banana","apple"))
+
+# Tapsiriq 2
+# name=input("Adivi Daxil et:")
+# surname=input("SoyAdivi Daxil et:")
+# print(" ".join([name,surname]))
+
+# Tapsiriq 3
+# cumle="Python Programing"
+# splitmt=cumle.split(" ")
+# print(splitmt[1])
+
+# print(cumle[-10:17])
+
+# Tapsiriq 4
+# email = str(input("Emailivizi Daxil et:"))
+
+# try:
+#     password = int(input("Paroluvuzu daxil edin:"))
+# except:
+#     print("Parol reqemden ibaret olmalidir")
+#     exit(1)
+
+# if "@gmail.com" in email:
+#     print("Successfull Login!")
+# else:
+#     print("Email @gmail.com ile bitmelidir")
